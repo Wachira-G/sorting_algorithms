569f93148dd3205b77ae9c6c5e6572a10ded3d28
Gabriel Wachira <gabbyk.com@gmail.com>
2023-07-26 20:02:53 +0300

    Fix task 1 swapping, logic errors, add 3-O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 
-void swap_node(listint_t **list, listint_t *node1, listint_t *node2);
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 
 /**
  * insertion_sort_list - implements the insertion sort in c
@@ -8,50 +8,70 @@ void swap_node(listint_t **list, listint_t *node1, listint_t *node2);
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp = NULL, *temp2 = NULL;
+	listint_t *current = (*list)->next;
 
-	if (list == NULL || *list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	temp = *list;
-	while (temp != NULL)
+	while (current != NULL)
 	{
-		if (temp->next)
-			temp2 = temp->next;
-		while (temp2 && temp->n > temp2->n)
-			swap_node(list, temp, temp2);
-		temp = temp->next;
+		listint_t *temp = current;
+
+		while (temp->prev != NULL && temp->prev->n > temp->n)
+		{
+			swap_nodes(list, temp->prev, temp);
+			temp = temp->next;
+			current = *list;
+		}
+
+		current = current->next;
 	}
 }
 
 
 /**
- * swap_node - swaps two nodes of a linked list
+ * swap_nodes - swaps two nodes of a linked list
  * @list: the doubly-linked list
  * @node1: the first node
  * @node2: the second node
  */
-void swap_node(listint_t **list, listint_t *node1, listint_t *node2)
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-	listint_t *temp = NULL;
+	if (node1 == NULL || node2 == NULL || node1 == node2)
+		return;
 
-	if (node1 == NULL || node2 == NULL ||
-	node1->next != node2 || node2->prev != node1)
-	return;
+	if (node1->next == node2 || node2->next == node1)
+	{
+		if (node1->next == node2)
+		{
+			node1->next = node2->next;
+			if (node2->next != NULL)
+				node2->next->prev = node1;
 
-	if (node1->prev != NULL)
-		node1->prev->next = node2;
-	else
-		*list = node2;
+			node2->prev = node1->prev;
+			if (node1->prev != NULL)
+				node1->prev->next = node2;
+			else
+				*list = node2;
 
-	if (node2->next != NULL)
-		node2->next->prev = node1;
+			node1->prev = node2;
+			node2->next = node1;
+		}
+		else
+		{ /* node2->next == node1 */
+			node2->next = node1->next;
+			if (node1->next != NULL)
+				node1->next->prev = node2;
 
-	temp = node2->next;
-	node1->next = node2->next;
-	node2->next = temp;
+			node1->prev = node2->prev;
+			if (node2->prev != NULL)
+				node2->prev->next = node1;
+			else
+				*list = node1;
 
-	temp = node1->prev;
-	node1->prev = node2;
-	node2->prev = temp;
+			node2->prev = node1;
+			node1->next = node2;
+		}
+	}
+	print_list(*list);
 }
